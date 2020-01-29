dafc177d50286d8c92ceab24437daa172dc4d5cc
Noah5 <wu.noah5@gmail.com>
2020-01-29 22:01:12 +0800

    篩選留言

--- reviews.py
+++ reviews.py
@@ -1,16 +1,23 @@
-# 算出留言的平均長度(字數)
+# 留言篩選
 data = []
 count = 0
-with open('reviews2.txt', 'r') as f:
+with open('reviews.txt', 'r') as f:
     for line in f:    #在讀取時是把一列資料讀進去，因此寫入data時也就是一筆留言
         data.append(line)    
         count += 1
-        if count % 1 == 0:
-            print(len(data))    #因為data是清單，因此len算來的是一百萬筆，而非所有文字的數量
+        if count % 100000 == 0:
+            print('已算到', len(data), '筆')    #因為data是清單，因此len算來的是一百萬筆，而非所有文字的數量
 
 # 這邊在計算總字數(空白也算)
 sum_len = 0
 for d in  data:
     sum_len= sum_len + len(d)
-print(sum_len)    #印出總字數
-print(sum_len/len(data))    #總字數除以筆數就是留言的平均字數
+print('留言總字數為', sum_len)    #印出總字數
+print('留言平均長度為', sum_len/len(data))    #總字數除以筆數就是留言的平均字數
+print('留言平均長度為', round(sum_len/len(data),2))    #round(a,b) 取a的資料四捨五入到小數第b位
+# 這邊在篩選留言字數小於100
+new = []   #建立一個新清單，用來裝過濾後的資料
+for d in data:
+	if len(d) < 100:    #如果留言字數小於100才裝進新清單
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')

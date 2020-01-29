9d99303d0e6c1365f0aab0755f83f8012ffc2ee4
Noah5 <wu.noah5@gmail.com>
2020-01-29 21:20:58 +0800

    002

--- reviews.py
+++ reviews.py
@@ -1,6 +1,10 @@
+# 因為每讀一筆就印出來一次計數，會導致速度很慢。
+# 因此修改讓他每數一千次印出
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
-print(len(data))    #印出有多少筆留言，一萬筆。
-print(data[0])    #印出第一筆
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
